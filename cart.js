a6683736b3ca67c7f66004908e46a43337bdc5db
Christian Cruz <c.cruzmarcial2001@gmail.com>
2023-05-04 10:10:07 -0500

    started JS

---
+++ cart.js
@@ -0,0 +1,30 @@
+window.addEventListener("load",function () {
+   calcCart();
+   var cart = document.forms.cart;
+   cart.elements.insurance.onchange = calcCart;
+
+   var shippingOptions = document.querySelectorAll('input[name="shipping"]');
+   for (var i = 0;i <= shippingOptions.length; i++) {
+       shippingOptions[i].onclick = calcCart;
+   }
+
+});
+
+function calcCart() {
+    var cart = document.forms.cart;
+    var insuranceCost = document.querySelector('[name="insurance"]:checked').value;
+    var shoeCost = document.querySelectorAll('[name="shoeCost"]').value;
+    cart.elements.cartTotal.value = formatUSCurrency(orderCost);
+
+   var shipCost = document.querySelector('input[name="shipping"]:checked').value;
+   cart.elements.shippingCost.value = formatNumber(shipCost);
+   cart.elements.cartTotal.value = formatNumber(shoeCost+shipCost+insuranceCost, 2);
+}
+function formatNumber(val, decimals) {
+    return val.toLocaleString(undefined, {minimumFractionDigits: decimals, 
+                                          maximumFractionDigits: decimals});
+ }
+ 
+ function formatUSCurrency(val) {
+    return val.toLocaleString('en-US', {style: "currency", currency: "USD"} );
+ }
